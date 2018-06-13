cbf85e2f69b5112c7de7d133a8a946a469524c54
griimnak <griimnak@gmail.com>
2018-06-12 20:13:41 -0400

    init

--- source/__init__.py
+++ source/__init__.py
@@ -7,36 +7,36 @@ from source.downloader import PyYTD
 init(autoreset=True)
 _pretty = Style.DIM + Fore.CYAN
 _count = 0
-_que = dict()
+_queue = dict()
 
 os.system("cls" if os.name == "nt" else "clear")
 print(_pretty + "Welcome to PyYTD".center(60))
 print(_pretty + "Python YouTube Downloader".center(60))
 print(_pretty + "https://github.com/griimnak\n".center(60))
 
-def add_to_que(link):
+def add_to_queue(link):
     global _count
     _count += 1
     if link != "":
-        _que[f'url{_count}'] = link
+        _queue[f'url{_count}'] = link
         print(f"\n{link} Successfully added to que list.\n")
-        print(_pretty+ f"(There are now {_count} urls qued.)\n")
+        print(_pretty+ f"(There are now {_count} urls queued.)\n")
         query_inputs()
     else:
-        print(_pretty + "Please enter a valid link to add to que.\n")
+        print(_pretty + "Please enter a valid link to add to queue.\n")
         query_inputs()
 
 def trigger_download():
-    global _que, _count
+    global _queue, _count
 
-    if not _que:
-        print("\nQue is empty, resorting to individual download mode.\n")
+    if not _queue:
+        print("\nQueue is empty, resorting to individual download mode.\n")
         link = input("Video link: ")
         dl = PyYTD(link)
         dl.download_video()
-    elif _que:
+    elif _queue:
         stop_watch = time.time()
-        for url in _que.items():
+        for url in _queue.items():
             print(
                 Style.DIM + Back.GREEN + Fore.WHITE +
                 "\n==> Working on " + url[1] + Style.RESET_ALL
@@ -51,7 +51,7 @@ def trigger_download():
 
     """ Clean up """
     _count = 0
-    _que = dict()
+    _queue = dict()
     query_inputs()
 
 """ Query inputs """
@@ -67,7 +67,7 @@ def query_inputs():
         print(_pretty+ "help - Displays this dialog.")
         print(_pretty+ "clear - Clears the console screen.")
         print(_pretty+ "exit - Exit the PyYTD application.")
-        print(_pretty+ "queadd :link: - Add a link to download que.")
+        print(_pretty+ "qadd :link: - Add a link to download que.")
         print(_pretty+ "dl - Triggers the video downloader.\n")
         query_inputs()
     elif _cmd == "clear":
@@ -77,9 +77,9 @@ def query_inputs():
         print(_pretty + "Thanks for using PyYTD.\n")
         exit()
 
-    elif _cmd[:6] == "queadd":
-        link = _cmd.replace(_cmd[:7], "")
-        add_to_que(link)
+    elif _cmd[:4] == "qadd":
+        link = _cmd.replace(_cmd[:5], "")
+        add_to_queue(link)
 
     elif _cmd == "dl":
         trigger_download()
